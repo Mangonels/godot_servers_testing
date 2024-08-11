1165354724a792c826eb8a6553a2364748e2721e
Mangonels <dylancalaflatham@gmail.com>
2024-08-11 20:26:50 +0200

    fixes for glb

--- scripts/body_constructor.gd
+++ scripts/body_constructor.gd
@@ -10,9 +10,17 @@ func _ready():
 	# appears with the same objects as the scene.
 	var scenario = get_world_3d().scenario
 	RenderingServer.instance_set_scenario(instance, scenario)
+	
 	# Add a mesh to it.
 	# Remember, keep the reference.
-	mesh = load("res://lukkys_props/barrel.glb")
+	var scene_resource: PackedScene = load("res://lukkys_props/barrel.glb") as PackedScene
+	var scene_instance: Node3D = scene_resource.instantiate()
+	# You can have multiple meshes per GLB IIRC.
+	# So i just get the first.
+	var mesh_instance: MeshInstance3D = scene_instance.get_child(0)
+	var mesh_resource: Mesh = mesh_instance.mesh
+	mesh = mesh_resource
+	
 	RenderingServer.instance_set_base(instance, mesh)
 	# Move the mesh around.
 	var xform = Transform3D(Basis(), Vector3(0, 0, 0))
